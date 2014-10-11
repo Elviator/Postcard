2d6c2ed930bd39359df1f8030bc0359a6b9581a8
David Lion <Lion@Davids-iMac.local>
2014-10-11 20:42:03 +0200

    Initial Commit

---
+++ Postkarte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postkarte
+//
+//  Created by David Lion on 11.10.14.
+//  Copyright (c) 2014 EASYCLEANAPPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
